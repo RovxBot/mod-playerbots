01bb4b774a104223615f596a148ef654106d7c93
Alex Dcnh <140754794+Wishmaster117@users.noreply.github.com>
2026-01-18 17:49:56 +0100

    codestyle

--- src/Ai/Base/Value/ItemUsageValue.cpp
+++ src/Ai/Base/Value/ItemUsageValue.cpp
@@ -694,7 +694,7 @@ static bool IsFallbackNeedReasonableForSpec(Player* bot, ItemTemplate const* pro
 
     if (traits.isRogue && stats.hasSTR)
         return false;
-	
+
     if (isStrengthOnly && !isStrengthUser())
         return false;
 
@@ -1024,7 +1024,7 @@ static ItemUsage AdjustUsageForCrossArmor(Player* bot, ItemTemplate const* proto
         if (!oldProto)
             continue;
 
-		if (oldProto->Class != ITEM_CLASS_ARMOR)
+        if (oldProto->Class != ITEM_CLASS_ARMOR)
             continue;
 
         if (oldProto->InventoryType != proto->InventoryType)
