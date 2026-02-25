0b07b7dea91e536e7f1ba8762043d1614043f8aa
rovxbot <sam@cooked.beer>
2026-02-25 10:43:24 +1030

    BWL: cache multiplier encounter state per tick

--- src/Ai/Raid/BlackwingLair/Multiplier/RaidBwlMultipliers.h
+++ src/Ai/Raid/BlackwingLair/Multiplier/RaidBwlMultipliers.h
@@ -11,7 +11,16 @@ public:
     float GetValue(Action* action) override;
 
 private:
+    void RefreshStateCache() const;
+
     BwlBossHelper helper;
+    mutable uint32 cacheMSTime = 0;
+    mutable bool cacheNefarianP1 = false;
+    mutable bool cacheNefarianP2 = false;
+    mutable bool cacheAnyBossEncounter = false;
+    mutable bool cacheDangerousTrashEncounter = false;
+    mutable bool cacheSeetherEnraged = false;
+    mutable bool cacheDeathTalonUndetected = false;
 };
 
 class BwlEncounterPositioningMultiplier : public Multiplier
@@ -21,7 +30,12 @@ public:
     float GetValue(Action* action) override;
 
 private:
+    void RefreshStateCache() const;
+
     BwlBossHelper helper;
+    mutable uint32 cacheMSTime = 0;
+    mutable bool cacheAnyBossEncounter = false;
+    mutable bool cacheChromaggusTimeLapseCast = false;
 };
 
 #endif
